554db3b051c5129e4f8d147b863b2df6a222bf86
DamoFD <voshall@yahoo.com>
2023-07-27 15:13:09 -0700

    First pricing card complete
    First pricing card complete

--- resources/views/components/pricing.blade.php
+++ resources/views/components/pricing.blade.php
@@ -1,61 +1,97 @@
+@php
+    $table1 = [
+        'Unlimited Revisions',
+        'Hands-free from start to finish',
+        'Basic Site (5pages)',
+        'Fully custom code',
+        'SEO optimized',
+        'Hosting, domain, and custom email',
+        'Google My Business/Google maps listings',
+        'Professionally maintained',
+    ];
+
+    $table2 = [
+        'Everything from basic',
+        'Unlimited pages*',
+        'Optimize your blog posts',
+        'Back link building strategies',
+        'Positive Review Builiding Strategies',
+        'Interactive websites',
+        'Ecommerce',
+        'Email list building strategies',
+        'Rank for many keywords',
+        'A/B Testing',
+        'Forms/Surveys',
+        'Full backend and database solutions',
+    ];
+
+    $table3 = [
+        'Everything from Premium',
+        'Employee Management',
+        'Customer Portals',
+        'SAAS solutions',
+        'Custom API endpoints',
+        'Custom CRM',
+        'Inventory Management',
+        'Scheduled SEO optimized blog posts',
+    ];
+@endphp
 <section class="pt-10" id="pricing">
     <h2 class="uppercase bg-gradient-to-r from-brand-primary-300 via-brand-primary-400 to-brand-primary-500 text-transparent bg-clip-text font-extrabold font-inter text-2xl text-center md:text-5xl lg:text-7xl">
         Pricing Plans
     </h2>
     <p class="text-brand-white font-inter px-8 py-4 text-center max-w-7xl md:text-xl md:mt-6 lg:text-3xl">Join our expanding community today! Avail our exclusive Premium plan and secure a lifetime subscription at an <strong class="uppercase text-brand-primary-500">Exceptional discount of 70%</strong>. Act now, as this incredible offer is valid for this month only!</p>
     <div class="w-full flex justify-between">
-        <div class="bg-brand-gray-500 border-2 border-brand-gray-600 rounded-xl p-4 w-1/3">
-            <h3 class="text-3xl text-brand-white font-inter font-extrabold">Basic</h3>
-            <p class="text-brand-gray-300 font-inter">I just want to get online.</p>
-            <p class="text-3xl text-brand-white font-inter font-extrabold">$99.99/Mo</p>
+        <div class="bg-brand-gray-500 border-2 border-brand-gray-600 rounded-xl flex flex-col justify-between p-4 w-1/3">
+            <div>
+                <h3 class="text-3xl text-brand-white font-inter font-extrabold">Basic</h3>
+                <p class="text-brand-gray-300 font-inter">I just want to get online.</p>
+                <p class="text-3xl text-brand-white font-inter font-extrabold">$99.99/Mo</p>
+                <ul>
+                    @foreach($table1 as $item)
+                    <li class="flex items-center space-x-2 mt-2">
+                        <img src="{{asset('svg/check.svg')}}" alt="check mark" />
+                        <p class="text-brand-gray-300 font-inter">{{$item}}</p>
+                    @endforeach
+                    </li>
+                </ul>
+            </div>
+            <a class="w-full block text-center font-extrabold text-brand-white bg-gradient-to-r from-brand-gray-400 to-brand-gray-900 px-4 py-2 rounded-xl" href="#">Get Basic</a>
+        </div>
+        <div class="rounded-xl bg-brand-primary-500 flex flex-col p-4 w-1/3">
+            <div class="flex w-full justify-between">
+                <h3 class="text-3xl text-brand-white font-inter font-extrabold">Premium</h3>
+                <div class="rounded-xl flex bg-brand-white bg-opacity-20 items-center px-2">
+                    <img class="w-4 h-4" src="{{asset('svg/star.svg')}}" alt="star">
+                    <p class="text-brand-white font-inter">Most Popular</p>
+                </div>
+            </div>
+            <p class="text-brand-white font-inter">I need functional software.</p>
+            <p class="text-brand-gray-300 font-inter"><del>$599.99</del>/Mo</p>
+            <p class="text-brand-white font-inter font-extrabold text-3xl">$199.99/Mo</p>
             <ul>
+                @foreach ($table2 as $item)
                 <li class="flex items-center space-x-2 mt-2">
-                    <img src="{{asset('svg/check.svg')}}" alt="check mark" />
-                    <p class="text-brand-gray-300 font-inter">Unlimited Revisions</p>
+                    <img class="text-brand-white" src="{{asset('svg/check-white.svg')}}" alt="check mark">
+                    <p class="text-brand-white font-inter">{{$item}}</p>
                 </li>
+                @endforeach
+            </ul>
+            <a href="#">Get Premium</a>
+        </div>
+        <div class="bg-brand-gray-500 border-2 border-brand-gray-600 rounded-xl p-4 w-1/3">
+            <h3 class="text-3xl text-brand-white font-inter font-extrabold">Enterprise</h3>
+            <p class="text-brand-gray-300 font-inter">I need a full business solution.</p>
+            <p class="text-3xl text-brand-white font-inter font-extrabold">Contact for Price</p>
+            <ul>
+                @foreach($table3 as $item)
                 <li class="flex items-center space-x-2 mt-2">
                     <img src="{{asset('svg/check.svg')}}" alt="check mark" />
-                    <p class="text-brand-gray-300 font-inter">Hands-free from start to finish</p>
-                </li>
-                <li class="flex items-center space-x-2 mt-2">
-                    <img src="{{asset('svg/check.svg')}}" alt="check mark" />
-                    <p class="text-brand-gray-300 font-inter">Basic Site (5 pages)</p>
-                </li>
-                <li class="flex items-center space-x-2 mt-2">
-                    <img src="{{asset('svg/check.svg')}}" alt="check mark" />
-                    <p class="text-brand-gray-300 font-inter">Fully custom code</p>
-                </li>
-                <li class="flex items-center space-x-2 mt-2">
-                    <img src="{{asset('svg/check.svg')}}" alt="check mark" />
-                    <p class="text-brand-gray-300 font-inter">SEO optimized</p>
-                </li>
-                <li class="flex items-center space-x-2 mt-2">
-                    <img src="{{asset('svg/check.svg')}}" alt="check mark" />
-                    <p class="text-brand-gray-300 font-inter">Hosting, domain, and custom email</p>
-                </li>
-                <li class="flex items-center space-x-2 mt-2">
-                    <img src="{{asset('svg/check.svg')}}" alt="check mark" />
-                    <p class="text-brand-gray-300 font-inter">Google My Business/Google maps listings</p>
-                </li>
-                <li class="flex items-center space-x-2 mt-2">
-                    <img src="{{asset('svg/check.svg')}}" alt="check mark" />
-                    <p class="text-brand-gray-300 font-inter">Professionally maintained</p>
+                    <p class="text-brand-gray-300 font-inter">{{$item}}</p>
+                @endforeach
                 </li>
             </ul>
             <a class="text-brand-white bg-gradient-to-r from-brand-gray-400 to-brand-gray-900 px-4 py-2 rounded-xl" href="#">Get Basic</a>
-        </div>
-        <div class="rounded-xl bg-brand-primary-500 flex flex-col p-4 w-1/3">
-            <h3>Premium</h3>
-            <p>I need functional software</p>
-            <p><del>$599.99</del>/Mo</p>
-            <p>$199.99/Mo</p>
-            <ul>
-                <li class="flex items-center space-x-2 mt-2">
-                    <img class="text-brand-white" src="{{asset('svg/check-white.svg')}}" alt="check mark">
-                    <p class="text-brand-white font-inter">Example</p>
-                </li>
-            </ul>
-            <a href="#"></a>
         </div>
     </div>
 </section>
