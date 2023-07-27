74b74e47146011fa31002bb4d73736d9dd45e5e8
DamoFD <voshall@yahoo.com>
2023-07-27 16:25:18 -0700

    pricing mobile friendly
    pricing mobile friendly

--- resources/views/components/pricing.blade.php
+++ resources/views/components/pricing.blade.php
@@ -36,12 +36,12 @@
         'Scheduled SEO optimized blog posts',
     ];
 @endphp
-<section class="pt-10" id="pricing">
-    <h2 class="uppercase bg-gradient-to-r from-brand-primary-300 via-brand-primary-400 to-brand-primary-500 text-transparent bg-clip-text font-extrabold font-inter text-2xl text-center md:text-5xl lg:text-7xl">
+<section class="py-10" id="pricing">
+    <h2 class="uppercase bg-gradient-to-r from-brand-primary-300 via-brand-primary-400 to-brand-primary-500 text-transparent bg-clip-text font-extrabold font-inter text-3xl text-center md:text-5xl lg:text-7xl">
         Pricing Plans
     </h2>
     <p class="text-brand-white font-inter px-8 py-4 text-center max-w-7xl md:text-xl md:mt-6 lg:text-3xl">Join our expanding community today! Avail our exclusive Premium plan and secure a lifetime subscription at an <strong class="uppercase text-brand-primary-500">Exceptional discount of 70%</strong>. Act now, as this incredible offer is valid for this month only!</p>
-    <div class="w-full flex flex-col items-center md:flex-row md:justify-between space-x-2">
+    <div class="w-full flex flex-col items-center md:flex-row md:justify-between md:space-x-2">
         <div class="mt-4 md:mt-24 bg-brand-gray-500 border-2 border-brand-gray-600 rounded-xl flex flex-col justify-between p-4 w-11/12 md:w-1/3">
             <div>
                 <h3 class="text-3xl text-brand-white font-inter font-extrabold">Basic</h3>
@@ -56,9 +56,9 @@
                     </li>
                 </ul>
             </div>
-            <a class="w-full block text-center font-extrabold text-brand-white bg-gradient-to-r from-brand-gray-400 to-brand-gray-900 px-4 py-2 rounded-xl" href="#">Get Basic</a>
+            <a class="w-full block text-center font-extrabold text-brand-white bg-gradient-to-r from-brand-gray-400 to-brand-gray-900 px-4 py-2 rounded-xl mt-8 md:mt-0" href="#">Get Basic</a>
         </div>
-        <div class="w-5/12 flex flex-col justify-between rounded-xl bg-brand-primary-500 p-4">
+        <div class="w-11/12 mt-4 md:mt-0 md:w-5/12 flex flex-col justify-between rounded-xl bg-brand-primary-500 p-4">
             <div>
                 <div class="flex w-full justify-between">
                     <h3 class="text-3xl text-brand-white font-inter font-extrabold">Premium</h3>
@@ -81,7 +81,7 @@
             </div>
             <a class="mt-8 bg-brand-white bg-opacity-20 w-full block rounded-xl text-center py-2 text-brand-white font-extrabold" href="#">Get Premium</a>
         </div>
-        <div class="mt-24 flex flex-col justify-between bg-brand-gray-500 border-2 border-brand-gray-600 rounded-xl p-4 w-1/3">
+        <div class="w-11/12 mt-4 md:mt-24 flex flex-col justify-between bg-brand-gray-500 border-2 border-brand-gray-600 rounded-xl p-4 md:w-1/3">
             <div>
                 <h3 class="text-3xl text-brand-white font-inter font-extrabold">Enterprise</h3>
                 <p class="text-brand-gray-300 font-inter">I need a full business solution.</p>
@@ -95,7 +95,7 @@
                     </li>
                 </ul>
             </div>
-            <a class="text-center font-extrabold text-brand-white bg-gradient-to-r from-brand-gray-400 to-brand-gray-900 px-4 py-2 rounded-xl" href="#">Get Basic</a>
+            <a class="text-center font-extrabold text-brand-white bg-gradient-to-r from-brand-gray-400 to-brand-gray-900 px-4 py-2 rounded-xl mt-8 md:mt-0" href="#">Get Basic</a>
         </div>
     </div>
 </section>
