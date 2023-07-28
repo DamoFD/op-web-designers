7b39c28ce05fc91c3d64a94f5262dfdc27cc30a5
DamoFD <voshall@yahoo.com>
2023-07-27 21:26:09 -0700

    key-elements mobile friendly
    key-elements mobile friendly

---
+++ resources/views/components/three-card.blade.php
@@ -0,0 +1,42 @@
+<section class="py-10 flex flex-col items-center lg:py-24" id="pricing">
+
+    <h2 class="uppercase bg-gradient-to-r from-brand-primary-300 via-brand-primary-400 to-brand-primary-500 text-transparent bg-clip-text font-extrabold font-inter text-3xl text-center md:text-5xl lg:text-7xl">
+        The Key Elements of Successful Web Design
+    </h2>
+    <p class="text-brand-white font-inter px-8 py-4 text-center max-w-7xl md:text-xl md:mt-6 lg:text-3xl">Web design is <strong>not</strong> a one-size-fits-all solution. Each website must be tailored to the specific needs of the business it represents. At your Reno web design company, the leading Reno, NV business in web design, we understand the <strong>key elements</strong> of successful digital marketing needs:</p>
+    <div class="flex flex-col items-center">
+
+        {{-- Card 1 --}}
+        <div class="rounded-lg flex w-11/12 flex-col items-center bg-gradient-to-r from-[#03717C] to-[rgba(0,0,0,0)] pt-2">
+            <div class="flex justify-between w-11/12 items-center">
+                <h3 class="font-extrabold font-inter text-brand-white text-2xl">User Experience (UX)</h3>
+                <img src="{{ asset('svg/arrow.svg') }}" alt="arrow">
+            </div>
+            <p class="w-11/12 font-inter text-brand-white">The user experience is <strong>paramount</strong>. If your website isn't easy to navigate or doesn't deliver the information your visitors are looking for, they'll <em>quickly</em> move on.</p>
+            <p class="w-11/12 font-inter text-brand-white">We focus on <em>intuitive</em> navigation, <em>clear</em> content layout, and <em>responsive</em> design to ensure your site meets and <strong>exceeds</strong> user expectations.</p>
+            <img class="w-11/12 mt-2" src="{{ asset('img/user-experience.png') }}" alt="custom web design services">
+        </div>
+
+        {{-- Card 2 --}}
+        <div class="rounded-lg flex mt-4 w-11/12 flex-col items-center bg-gradient-to-r from-[#6A1983] to-[rgba(0,0,0,0)] pt-2">
+            <div class="flex justify-between w-11/12 items-center">
+                <h3 class="font-extrabold font-inter text-brand-white text-2xl">Visual Appeal<h3>
+                <img src="{{ asset('svg/arrow.svg') }}" alt="arrow">
+            </div>
+            <p class="w-11/12 font-inter text-brand-white"><strong>First impressions matter</strong>, and in the digital world, your website is often the first point of contact between you and <em>potential customers</em>.</p>
+            <p class="w-11/12 font-inter text-brand-white">Our team ensures your site is <em>visually appealing</em>, utilizing color psychology, typography, and layout principles to create an <strong>aesthetically pleasing</strong> and engaging user experience.</p>
+            <img class="w-11/12 mt-2" src="{{ asset('img/visual-appeal.png')}}" alt="custom web design services">
+        </div>
+
+        {{-- Card 3 --}}
+        <div class="rounded-lg mt-4 flex w-11/12 flex-col items-center bg-gradient-to-r from-[#272ABB] to-[rgba(0,0,0,0)] pt-2">
+            <div class="flex justify-between w-11/12 items-center">
+                <h3 class="font-extrabold font-inter text-brand-white text-2xl">Mobile Responsiveness</h3>
+                <img src="{{ asset('svg/arrow.svg') }}" alt="arrow">
+            </div>
+            <p class="w-11/12 font-inter text-brand-white">As mobile browsing becomes <em>increasingly</em> prevalent, it's <strong>crucial</strong> for your website to perform well on a variety of devices.</p>
+            <p class="w-11/12 font-inter text-brand-white">Our team ensures your website design is responsive, meaning it will look and function <strong>seamlessly</strong> whether your visitors are on a <em>desktop, tablet, or smartphone</em>.</p>
+            <img class="w-11/12 mt-2" src="{{ asset('img/mobile-responsiveness.png') }}" alt="custom web design services">
+        </div>
+    </div>
+</section>
